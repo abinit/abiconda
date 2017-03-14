0fa1e160f6029c2064005b1bf8335b6d579bf880
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-03-15 00:43:51 +0100

    Pass openblas to atompaw

--- recipes/atompaw/build.sh
+++ recipes/atompaw/build.sh
@@ -4,8 +4,12 @@
 XC_INCS="-I${PREFIX}/include"
 XC_LIBS="-L${PREFIX}/lib -lxcf90 -lxc"
 
+# Open BLAS
+LINALG_LIBS="-L${PREFIX}/lib -lopenblas -lpthread"
+
 ./configure --prefix=$PREFIX \
-   --enable-libxc --with-libxc-incs="${XC_INCS}" --with-libxc-libs="${XC_LIBS}"
+   --enable-libxc --with-libxc-incs="${XC_INCS}" --with-libxc-libs="${XC_LIBS}" \
+   --with-linalg-libs="${LINALG_LIBS}"
 
 make -j1  # parallel make does not work
 make install
