d0f3263b5b2075038f1654d7d8a56da6ba1c2a30
Matteo Giantomassi <matteo.giantomassi@uclouvain.be>
2017-03-14 19:34:53 +0100

    change sha

--- build_linux.sh
+++ build_linux.sh
@@ -1,9 +1,13 @@
 set -e
-cd recipes
 
-for pkg in `ls -d *`
-do
-    cmd="conda build ${pkg}"
-    echo Executing ${cmd}
-    ${cmd}
-done
+conda build recipes/fftw
+conda build recipes/libxc
+conda build recipes/abinit
+
+#cd recipes
+#for pkg in `ls -d *`
+#do
+#    cmd="conda build ${pkg}"
+#    echo Executing ${cmd}
+#    ${cmd}
+#done
