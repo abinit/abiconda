c7cebfcdec83c4b3c2407d6d1e726226e9b5ac02
gmatteo <gmatteo@gmail.com>
2017-03-15 14:23:17 +0100

    Comment code needed for OsX with old conda versions

--- recipes/abinit/build.sh
+++ recipes/abinit/build.sh
@@ -2,16 +2,16 @@
 #https://github.com/conda-forge/fftw-feedstock/blob/ad8bc8c5661447db646ff3c48fed3dd4a23edc5a/recipe/build.sh
 
 # Depending on our platform, shared libraries end with either .so or .dylib
-if [[ `uname` == 'Darwin' ]]; then
-    # Use homebrew
-    export CC=gcc-5
-    export FC=gfortran-5
-    #export CC=gcc
-    #export FC=gfortran
-else
-    export CC=gcc
-    export FC=gfortran
-fi
+#if [[ `uname` == 'Darwin' ]]; then
+#    # Use homebrew
+#    export CC=gcc-5
+#    export FC=gfortran-5
+#    #export CC=gcc
+#    #export FC=gfortran
+#else
+#    export CC=gcc
+#    export FC=gfortran
+#fi
 
 
 # conda-installed gfortran on OSX: builds fail at runtime 
@@ -31,27 +31,27 @@ export FCFLAGS="-O0 -g -ffree-line-length-none -Wl,-rpath,${CONDA_PREFIX}/lib"
 # see https://gcc.gnu.org/onlinedocs/gcc-4.8.3/gcc/Code-Gen-Options.html#Code-Gen-Options
 
 #LIBGFORTRAN_DIR=~/anaconda2/lib
-LIBGFORTRAN_DIR=/usr/local/opt/gcc/lib/gcc/5
-LIBGFORTRAN_NAME=libgfortran.3.dylib
+#LIBGFORTRAN_DIR=/usr/local/opt/gcc/lib/gcc/5
+#LIBGFORTRAN_NAME=libgfortran.3.dylib
 
 #LIBGCC_S_DIR=~/anaconda2/lib
-LIBGCC_S_DIR=/usr/local/lib/gcc/5
-LIBGCC_S_NAME=libgcc_s.1.dylib 
+#LIBGCC_S_DIR=/usr/local/lib/gcc/5
+#LIBGCC_S_NAME=libgcc_s.1.dylib 
 
 #LIBQUADMATH_DIR=~/anaconda2/lib
-LIBQUADMATH_DIR=/usr/local/opt/gcc/lib/gcc/5
-LIBQUADMATH_NAME=libquadmath.0.dylib
+#LIBQUADMATH_DIR=/usr/local/opt/gcc/lib/gcc/5
+#LIBQUADMATH_NAME=libquadmath.0.dylib
 
-LIBGFORTRAN_PATH=${LIBGFORTRAN_DIR}/${LIBGFORTRAN_NAME}
-LIBGCC_S_PATH=${LIBGCC_S_DIR}/${LIBGCC_S_NAME}
-LIBQUADMATH_PATH=${LIBQUADMATH_DIR}/${LIBQUADMATH_NAME}
+#LIBGFORTRAN_PATH=${LIBGFORTRAN_DIR}/${LIBGFORTRAN_NAME}
+#LIBGCC_S_PATH=${LIBGCC_S_DIR}/${LIBGCC_S_NAME}
+#LIBQUADMATH_PATH=${LIBQUADMATH_DIR}/${LIBQUADMATH_NAME}
 
-if [[ `uname` == 'Darwin' ]]; then
-    mkdir -p ${PREFIX}/lib
-    cp ~/anaconda2/lib/${LIBGFORTRAN_NAME} ${PREFIX}/lib/${LIBGFORTRAN_NAME}
-    cp ~/anaconda2/lib/${LIBQUADMATH_NAME} ${PREFIX}/lib/${LIBQUADMATH_NAME}
-    cp ~/anaconda2/lib/${LIBGCC_S_NAME}    ${PREFIX}/lib/${LIBGCC_S_NAME}
-fi
+#if [[ `uname` == 'Darwin' ]]; then
+#    mkdir -p ${PREFIX}/lib
+#    cp ~/anaconda2/lib/${LIBGFORTRAN_NAME} ${PREFIX}/lib/${LIBGFORTRAN_NAME}
+#    cp ~/anaconda2/lib/${LIBQUADMATH_NAME} ${PREFIX}/lib/${LIBQUADMATH_NAME}
+#    cp ~/anaconda2/lib/${LIBGCC_S_NAME}    ${PREFIX}/lib/${LIBGCC_S_NAME}
+#fi
 
 # https://software.intel.com/en-us/articles/intel-mkl-link-line-advisor
 # Linux-GCC with MKL dynamically
@@ -109,29 +109,29 @@ make check
 # Install binaries (don't copy test files to reduce size of the package)
 make install-exec
 
-declare -a ABINIT_BINARIES=(
-	"abinit" "band2eps" "cut3d" "ioprof" "mrgdv" "ujdet"
-	"bsepostproc" "lapackprof" "mrggkk" "vdw_kernelgen"
-	"aim" "fftprof" "macroave" "mrgscr" 
-	"anaddb" "conducti" "fold2Bloch" "mrgddb" "optic"
-)
+#declare -a ABINIT_BINARIES=(
+#	"abinit" "band2eps" "cut3d" "ioprof" "mrgdv" "ujdet"
+#	"bsepostproc" "lapackprof" "mrggkk" "vdw_kernelgen"
+#	"aim" "fftprof" "macroave" "mrgscr" 
+#	"anaddb" "conducti" "fold2Bloch" "mrgddb" "optic"
+#)
+#
+#if [[ `uname` == 'Darwin' ]]; then
+#    for bname in "${ABINIT_BINARIES[@]}" 
+#    do
+#        otool -L ${PREFIX}/bin/${bname}
+#        install_name_tool \
+#          -change ${LIBGCC_S_PATH} ${CONDA_PREFIX}/lib/${LIBGCC_S_NAME} \
+#          -change ${LIBQUADMATH_PATH} ${CONDA_PREFIX}/lib/${LIBQUADMATH_NAME} \
+#          -change ${LIBGFORTRAN_PATH} ${CONDA_PREFIX}/lib/${LIBGFORTRAN_NAME} \
+#          ${PREFIX}/bin/${bname}
+#        otool -L ${PREFIX}/bin/${bname}
+#    done 
+#fi 
 
-if [[ `uname` == 'Darwin' ]]; then
-    for bname in "${ABINIT_BINARIES[@]}" 
-    do
-        otool -L ${PREFIX}/bin/${bname}
-        install_name_tool \
-          -change ${LIBGCC_S_PATH} ${CONDA_PREFIX}/lib/${LIBGCC_S_NAME} \
-          -change ${LIBQUADMATH_PATH} ${CONDA_PREFIX}/lib/${LIBQUADMATH_NAME} \
-          -change ${LIBGFORTRAN_PATH} ${CONDA_PREFIX}/lib/${LIBGFORTRAN_NAME} \
-          ${PREFIX}/bin/${bname}
-        otool -L ${PREFIX}/bin/${bname}
-    done 
-fi 
 
-
-unset FC
-unset CC
+#unset FC
+#unset CC
 unset CFLAGS
 unset FCFLAGS
 unset LDFLAGS
